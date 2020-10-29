072343a8f59646c6bbc815fcbaa5c475bb57fb6f
Stefanie Serpa <smserpa@gmail.com>
2020-10-29 08:12:02 -0700

    designed Workout.js file following structure of seed.js to create schema for date and workout

---
+++ models/Workout.js
@@ -0,0 +1,54 @@
+// reference to mongoose
+const mongoose = require("mongoose");
+
+// create new schema
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            duration: {
+                type: Number,
+                required: true,
+                min: 1,
+                default: 20,
+            },
+            weight: {
+                type: Number,
+                min: 20,
+            },
+            reps: {
+                type: Number,
+                min: 1,
+                default: 10,
+            },
+            sets: {
+                type: Number,
+                min: 1,
+                default: 4,
+            },
+            distance: {
+                type: Number,
+                min: 1,
+                default: 4,
+            },
+
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
