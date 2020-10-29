04469ef7eb0931472e7bc0957af5713e0fa49e8a
Stefanie Serpa <smserpa@gmail.com>
2020-10-29 08:10:42 -0700

    created server.js file

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Workout = require("./Develop/models/Workout");
+
 
 const PORT = process.env.PORT || 3000;
+
+const Workout = require("./models/Workout"); 
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );  
+
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
